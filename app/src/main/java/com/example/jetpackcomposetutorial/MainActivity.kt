75683d11f62bd3033dcd8b14ed9014f4356f1e0c
hayashi <hayashi@yappli.co.jp>
2021-03-14 18:42:14 +0900

    Add image

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.jetpackcomposetutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -23,9 +26,13 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
+    val image: Painter = painterResource(id = R.drawable.header)
+
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
+        Image(painter = image, contentDescription = null)
+
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
